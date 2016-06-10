8baf8372e8a09ab2878ce871e9177f5a37561c97
Alex Bird <bird@grandrounds.com>
2016-06-10 15:28:56 -0700

    add changes to get working on uat

--- examples/client.rb
+++ examples/client.rb
@@ -1,15 +1,16 @@
 require 'as2'
-# require 'pry'
+
+ENV['AS2_HOST'] || raise('must set AS2_HOST')
 
 As2.configure do |conf|
   conf.name = 'MyClient'
-  conf.url = 'http://localhost:8080/as2/HttpReceiver'
+  conf.url = "http://#{ENV['AS2_HOST']}/as2"
   conf.certificate = 'test/certificates/client.crt'
   conf.pkey = 'test/certificates/client.key'
   conf.domain = 'mydomain.com'
   conf.add_partner do |partner|
     partner.name = 'MyServer'
-    partner.url = 'http://localhost:3000/as2'
+    partner.url = "http://#{ENV['AS2_HOST']}/as2"
     partner.certificate = 'test/certificates/server.crt'
   end
 end
--- examples/server.rb
+++ examples/server.rb
@@ -1,15 +1,17 @@
 require 'as2'
 require 'rack'
 
+ENV['AS2_HOST'] || raise('must set AS2_HOST')
+
 As2.configure do |conf|
   conf.name = 'MyServer'
-  conf.url = 'http://localhost:3000/as2'
+  conf.url = "http://#{ENV['AS2_HOST']}/as2"
   conf.certificate = 'test/certificates/server.crt'
   conf.pkey = 'test/certificates/server.key'
   conf.domain = 'mydomain.com'
   conf.add_partner do |partner|
     partner.name = 'MyClient'
-    partner.url = 'http://localhost:8080/as2/HttpReceiver'
+    partner.url = "http://#{ENV['AS2_HOST']}/as2"
     partner.certificate = 'test/certificates/client.crt'
   end
 end
@@ -19,14 +21,23 @@ handler = As2::Server.new do |filename, body|
   puts "FILENAME: #{filename}"
   puts
   puts body
+
+  file = File.new("/tmp/#{filename}", 'w+')
+  file.write(body)
+  file.close
 end
 
 builder = Rack::Builder.new do
   use Rack::CommonLogger
+
+  map '/status' do
+    run ->(env) {[200, {'Content-Type' => 'text/plain'}, ['Okay']] }
+  end
+
   map '/as2' do
     run handler
   end
 end
 
 puts "As2 version: #{As2::VERSION}"
-Rack::Handler::Thin.run builder, Port: 3000
+Rack::Handler::Thin.run builder, Host: '0.0.0.0', Port: 3000
